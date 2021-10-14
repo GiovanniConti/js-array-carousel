978311432ec919bf04f222de9d8e00a3b38a557f
GiovanniConti <giovanni.conti@live.it>
2021-10-14 18:45:02 +0200

    Fixed layout with img title and news added with JS

--- js/script.js
+++ js/script.js
@@ -24,11 +24,41 @@ const text = [
 
 
 const containerImg = document.querySelector(".container-img");
+const containerOverlay = document.querySelector(".container-overlay");
 const nav = document.querySelector(".nav");
+
+let currentIndex = 0;
 
 for (let i = 0; i < items.length; i++) {
     const currentItem = items[i];
+    const currentTitle = title[i];
+    const currentNews = text[i];
+
+    console.log(currentItem);
+    console.log(currentTitle);
+    console.log(currentNews);
+
+    let classActiveImg = "";
+    let classActiveNav = "";
+
+    if(i === currentIndex){
+        classActiveImg = "active";
+        
+    }
     
-    containerImg.innerHTML += `<img class="img" src="${currentItem}" alt="img${i}">`
-    nav.innerHTML += `<img class="img-nav" src="${currentItem}" alt="img${i}">`
+    containerImg.innerHTML += `<img class="img ${classActiveImg}" src="${currentItem}" alt="img${i}">`;
+    containerOverlay.innerHTML += `<h1 class="title ${classActiveImg}">${currentTitle}</h1>`;
+    containerOverlay.innerHTML += `<p class="news ${classActiveImg}">${currentNews}</p>`;
+    nav.innerHTML += `<img class="img-nav ${classActiveNav}" src="${currentItem}" alt="img${i}">`;
 }
+
+const chevronUp = document.querySelector(".chevron-up");
+const chevronDown = document.querySelector(".chevron-down");
+
+chevronUp.addEventListener("click", function(){
+    const activeImage = containerImg.querySelector(".active");
+});
+
+chevronDown.addEventListener("click", function(){
+
+});
